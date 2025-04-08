39fd8900e6876f1f9907a1d423a9b76d6f5fa02c
Riakol <inspi47@gmail.com>
2025-04-08 19:05:03 +0500

    first commit

---
+++ java/com/example/pomodoro/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.pomodoro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.pomodoro.ui.theme.PomodoroTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PomodoroTheme {
+
+            }
+        }
+    }
+}
+
